777a7a5928ef3a93f2f7a87181a4b0ebdca9b3e7
Gabriel Lemos <gabrieldebritolemos@gmail.com>
2022-01-27 11:41:35 -0300

    Implemented result screen

---
+++ src/pages/Results/index.tsx
@@ -0,0 +1,56 @@
+import React, { useMemo } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+import * as Card from '../../components/Card'
+import { Button } from '../../components/Button'
+import { Container, Result } from './Styles'
+import { ITrivia } from '../../interface/ITrivia'
+
+interface IResults {
+  questions: ITrivia[]
+  results: boolean[]
+}
+
+const Results = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const { questions, results } = useMemo(() => {
+    return location.state as IResults
+  }, [location.state])
+
+  const score = useMemo(() => {
+    return results.reduce((acc, isCorrect) => {
+      if (isCorrect) {
+        acc++
+      }
+      return acc
+    }, 0)
+  }, [results])
+
+  return (
+    <Container>
+      <p className="score">
+        You scored {score} / {questions.length}
+      </p>
+      <Card.Container className="noPaddingX">
+        <table>
+          <tbody>
+            {questions.map((question, index) => {
+              const answer = results[index] ? '✓' : '✗'
+              return (
+                <Result isCorrect={results[index]}>
+                  <td className="result__answer">{answer}</td>
+                  <td className="result__content">{question.question}</td>
+                </Result>
+              )
+            })}
+          </tbody>
+        </table>
+      </Card.Container>
+      <Button onClick={() => navigate('/')}>PLAY AGAIN?</Button>
+    </Container>
+  )
+}
+
+export default Results
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
 import Challenge from './Challenge'
+import Results from './Results'
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/challenge" element={<Challenge />} />
+        <Route path="/results" element={<Results />} />
       </Routes>
     </BrowserRouter>
   )
